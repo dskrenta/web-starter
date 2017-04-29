c14a224c376188ca2062aed64fb4a3989a3ab685
dskrenta <dskrenta@gmail.com>
2017-04-29 03:40:30 -0700

    graphql start

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const graphqlHTTP = require('koa-graphql');
+const { buildSchema } = require('graphql');
 
-// response
+const app = new Koa();
+const router = new Router();
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  }
+};
+
+router.all('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
+
 
 app.listen(3000);
