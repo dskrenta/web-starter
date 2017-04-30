0e7cc3f82a31b05892eea3bc26f93c2557675157
dskrenta <dskrenta@gmail.com>
2017-04-30 16:12:11 -0700

    basic todo query graphql schema

--- server/index.js
+++ server/index.js
@@ -2,23 +2,44 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const graphqlHTTP = require('koa-graphql');
 const { buildSchema } = require('graphql');
+const crypto = require('crypto');
 
 const app = new Koa();
 const router = new Router();
 
+class TodoItem {
+  constructor(id, content, created) {
+    this.id = id;
+    this.content = content;
+    this.created = created;
+  }
+}
+
+function generateID() {
+  return crypto.randomBytes(10).toString('hex');
+}
+
+const fakeDB = [
+  new TodoItem(generateID(), 'Hello, todo!', new Date()),
+  new TodoItem(generateID(), 'Hang up laundry.', new Date()),
+  new TodoItem(generateID(), 'Order Chinese food from Door Dash.', new Date())
+];
+
 const schema = buildSchema(`
+  type TodoItem {
+    id: String!
+    content: String!
+    created: String!
+  }
+
   type Query {
-    hello: String,
-    stuff: String
+    getTodoItems: [TodoItem]
   }
 `);
 
 const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-  stuff: () => {
-    return 'more stuff and even more!';
+  getTodoItems: () => {
+    return fakeDB;
   }
 };
 
